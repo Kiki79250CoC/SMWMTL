9fae49174d511594846a00ba363f13d3418d95df
Kylian Nicouleaud <54137141+Kiki79250CoC@users.noreply.github.com>
2022-05-01 04:43:11 +0200

    Branch Dev Migration from Codebase 2.2x to 2.3x

--- SMW MultiTool Launcher/My Project/Resources.Designer.vb
+++ SMW MultiTool Launcher/My Project/Resources.Designer.vb
@@ -100,7 +100,7 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Recherche une chaîne localisée semblable à Montréal.
+        '''  Recherche une chaîne localisée semblable à Laval.
         '''</summary>
         Friend ReadOnly Property APP_CODENAME() As String
             Get
@@ -118,7 +118,7 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Recherche une chaîne localisée semblable à 1600.00.
+        '''  Recherche une chaîne localisée semblable à 1700.00.
         '''</summary>
         Friend ReadOnly Property APP_VERSION_BUILD() As String
             Get
@@ -127,7 +127,7 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Recherche une chaîne localisée semblable à 2.20.1600.00-rtmbld-01.
+        '''  Recherche une chaîne localisée semblable à 2.20.1700.00-am1bld-01.
         '''</summary>
         Friend ReadOnly Property APP_VERSION_COMPLETE() As String
             Get
@@ -1600,7 +1600,7 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Recherche une chaîne localisée semblable à Stable.
+        '''  Recherche une chaîne localisée semblable à Alpha (Milestone 1).
         '''</summary>
         Friend ReadOnly Property RELEASE_STATE() As String
             Get
