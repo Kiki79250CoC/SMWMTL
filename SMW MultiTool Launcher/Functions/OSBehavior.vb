de33be9581a45fd3bacda0b9092536ca3f464a40
Kiki79250CoC <54137141+Kiki79250CoC@users.noreply.github.com>
2025-03-24 18:00:06 +0100

    Stuff™

---
+++ SMW MultiTool Launcher/Functions/OSBehavior.vb
@@ -0,0 +1,35 @@
+﻿
+Public Class ClientOSInfo
+    Public Shared ReadOnly KVer As Version = Version.Parse(FileVersionInfo.GetVersionInfo($"{Environment.SystemDirectory}\NTOSKrnl.exe").ProductVersion)
+    Public Shared ReadOnly HALVer As Version = Version.Parse(FileVersionInfo.GetVersionInfo($"{Environment.SystemDirectory}\HAL.dll").ProductVersion)
+    Public Shared ReadOnly W32kVer As Version = Version.Parse(FileVersionInfo.GetVersionInfo($"{Environment.SystemDirectory}\Win32K.sys").ProductVersion)
+    Public Shared ReadOnly WLVer As Version = Version.Parse(FileVersionInfo.GetVersionInfo($"{Environment.SystemDirectory}\WinLogon.exe").ProductVersion)
+
+End Class
+
+Public Class OSBehavior
+
+    Public Shared Function IsOSSupported()
+
+        ' If one of these checks fails, it means that the OS is not supported.
+        ' While CltOSVer is easy to bypass, KVer and HALVer are linked to the
+        ' physical OS system files, and I'm not sure anyone wants to Reshack
+        ' system files' version number.
+        '
+        ' The check of the bootloader could have been a possibility, but it would
+        ' cause a problem of false positive if a NTLDR-based OS is installed 
+        ' aside of a modern version of Windows.
+
+        Dim RequiredOSVersion As New Version(6, 0, 6002)
+
+        Dim CltOSVerChk As Boolean = RequiredOSVersion.CompareTo(SystemInterop.CltOSV)
+        Dim KVerChk As Boolean = RequiredOSVersion.CompareTo(ClientOSInfo.KVer)
+        Dim HALVerChk As Boolean = RequiredOSVersion.CompareTo(ClientOSInfo.HALVer)
+        Dim W32kVerChk As Boolean = RequiredOSVersion.CompareTo(ClientOSInfo.W32kVer)
+        Dim WinLogonVerChk As Boolean = RequiredOSVersion.CompareTo(ClientOSInfo.WLVer)
+
+        Return CltOSVerChk AndAlso KVerChk AndAlso HALVerChk AndAlso W32kVerChk AndAlso WinLogonVerChk
+
+    End Function
+
+End Class
---
+++ SMW MultiTool Launcher/Functions/OSInteract.vb
@@ -0,0 +1,18 @@
+﻿
+Imports System.IO
+
+Public Class OSInteract
+
+    Public Shared Function PELinkerTimestamp(Path As String) As Date
+        Dim FileB(2047) As Byte
+        Dim FileS As Stream = Nothing
+        Try
+            FileS = New FileStream(Path, FileMode.Open, FileAccess.Read)
+            FileS.Read(FileB, 0, 2048)
+        Finally
+            If FileS IsNot Nothing Then FileS.Close()
+        End Try
+        Return New DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(BitConverter.ToInt32(FileB, BitConverter.ToInt32(FileB, 60) + 8)).AddHours(TimeZone.CurrentTimeZone.GetUtcOffset(New DateTime(1970, 1, 1, 0, 0, 0)).Hours)
+    End Function
+
+End Class
